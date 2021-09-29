90bc88d441d2f8b536e603d2015ba272c17e81a6
Dan Benson <dbenson3091@gmail.com>
2021-09-28 19:25:40 -0500

    updated class functions

--- markov.js
+++ markov.js
@@ -1,8 +1,6 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
-
   /** build markov machine; read in text.*/
 
   constructor(text) {
@@ -19,23 +17,61 @@ class MarkovMachine {
   makeChains() {
     let chain = {};
     for (let i = 0; i < this.words.length; i++) {
-      if (chain[this.words[i]] === undefined) {
-        chain[this.words[i]] = [this.words[i+1]];
+      let word = this.words[i];
+      let nextWord = this.words[i + 1];
+
+      if (chain[word] === undefined) {
+        chain[word] = [nextWord];
       } else {
-        chain[this.words[i]].push(this.words[i+1]);
+        chain[word].push(nextWord);
       }
     }
     console.log("chain:", chain);
     return chain;
   }
-  
-
 
   /** return random text from chains */
 
   getText(numWords = 100) {
+    let key = Object.keys(this.chain);
+    let randomText = this.getRandomIndex(key);
+
+    let string = "";
+    // let firstWord = key[randomText];
+    // string += `${firstWord} `;
+
+    for (let i = 0; i < numWords; i++) {
+      let secondWord = "";
+      let firstWord;
+      if (i === 0 || secondWord === undefined) {
+        firstWord = key[randomText];
+        string += `${firstWord} `;
+        secondWord = this.chain[firstWord];
+      }
+      let getTextChain = this.chain[firstWord][this.getRandomIndex(secondWord)];
+      debugger;
+
+      if (getTextChain === undefined) {
+        debugger;
+        string += ". ";
+        secondWord = key[this.getRandomIndex(key)];
+      } else {
+        string += `${getTextChain} `;
+        secondWord = getTextChain;
+      }
+    }
+
+    debugger;
+    console.log(string);
+    return string;
     // MORE CODE HERE
+  }
+
+  getRandomIndex(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return randomIndex;
   }
 }
 
 let mm = new MarkovMachine("the cat in the hat");
+mm.getText();
